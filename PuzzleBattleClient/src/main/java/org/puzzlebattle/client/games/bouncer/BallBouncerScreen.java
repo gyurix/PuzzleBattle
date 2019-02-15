a14a486c03e188ef58d11e35494551e3d500e9ac
gyurix <barathgy@gmail.com>
2019-02-15 23:16:14 +0100

    Applied ball bouncer background

--- PuzzleBattleClient/src/main/java/org/puzzlebattle/client/games/bouncer/BallBouncerScreen.java
+++ PuzzleBattleClient/src/main/java/org/puzzlebattle/client/games/bouncer/BallBouncerScreen.java
@@ -1,6 +1,8 @@
 package org.puzzlebattle.client.games.bouncer;
 
 import javafx.scene.Scene;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.stage.Stage;
 import org.puzzlebattle.client.screen.AbstractScreen;
 
@@ -12,6 +14,7 @@ public class BallBouncerScreen extends AbstractScreen {
   public BallBouncerScreen(Stage stage, BouncerGame game) {
     super(stage);
     this.game = game;
+    pane.setBackground(new Background(new BackgroundFill(game.getSettings().getBackgroundColor(), null, null)));
     pane.getChildren().addAll(game.getBall(), game.getEnemy().getBouncer(), game.getYou().getBouncer());
     scheduleAtFixedRate(16, game::tick);
   }
